7944b9eac6be1fee47c6ec267e178774309630eb
cxcer-lz <zhwjleo@gmail.com>
2024-07-05 20:11:56 +0800

    Update Bigbank.sol

--- day4/Bigbank.sol
+++ day4/Bigbank.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
 
+import "./bank.sol";
+
+contract BigBank is Bank{
+ 
+    modifier Depositminamount(){
+        require(msg.value>0.001 ether,"the doposit amount is little!");
+        _;
+    } 
+
+    function Deposit()public virtual payable override Depositminamount{
+        super.Deposit();
+    }
+
+    function transferowner(address newowner)public onlyAdmin{
+        require(msg.sender!= address(0),'zero addr!');
+        admin= newowner;
+    }
+
+}
