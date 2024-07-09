70dd18daa87894494ffcb7385b71d17f455b8582
cxcer-lz <zhwjleo@gmail.com>
2024-07-09 20:16:37 +0800

    Update tokencallback.sol

--- day6/token/tokencallback.sol
+++ day6/token/tokencallback.sol
@@ -1,1 +1,136 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+library SafeMath {
+
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b);
+
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b > 0); // Solidity only automatically asserts when dividing by 0
+    uint256 c = a / b;
+
+    return c;
+  }
+
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b <= a);
+    uint256 c = a - b;
+
+    return c;
+  }
+
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a);
+
+    return c;
+  }
+
+ 
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    require(b != 0);
+    return a % b;
+  }
+}
+
+interface TokenRecipient{
+  function tokensReceived(address sender,uint256 amount) external returns (bool);
+}
+
+
+
+contract BaseERC20 {
+    string public name; 
+    string public symbol; 
+    uint8 public decimals; 
+    using SafeMath for uint;
+    uint256 public totalSupply; 
+
+    mapping (address => uint256) balances; 
+
+    mapping (address => mapping (address => uint256)) allowances; 
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(){
+        name="BaseERC20";
+        symbol="BERC20";
+        decimals=18;
+        totalSupply=100000000*10**decimals;
+        balances[msg.sender] = totalSupply;  
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        // write your code here
+        return balances[_owner];
+
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(_value<=balances[msg.sender],"ERC20: transfer amount exceeds balance");
+        require(_to != address(0));
+        balances[msg.sender]=balances[msg.sender].sub(_value);
+        balances[_to]=balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);  
+        return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(_value<=balances[_from],"ERC20: transfer amount exceeds balance");
+        require(_value<=allowances[_from][msg.sender],"ERC20: transfer amount exceeds allowance");
+        require(_to!= address(0));
+        balances[_from]=balances[_from].sub(_value);
+        balances[_to]=balances[_to].add(_value);
+        allowances[_from][msg.sender]=allowances[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(_spender != address(0));
+        allowances[msg.sender][_spender]=_value;
+
+        emit Approval(msg.sender, _spender, _value); 
+        return true; 
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {   
+        // write your code here
+        return allowances[_owner][_spender];
+
+    }
+
+    function isContract(address addr) public view returns (bool) {
+      uint256 size;
+      assembly { size := extcodesize(addr) }
+      return size > 0;
+    }
+    //通过这个函数向tokenbank合约存入代币，无需approve
+    function TransferAndCall(address recipient, uint256 amount)external returns (bool) {
+      require(amount<=balances[msg.sender],'amount exceeds balance');
+        transfer(recipient,amount);
+       if (isContract(recipient)) {
+            bool rv = TokenRecipient(recipient).tokensReceived(msg.sender, amount);
+            require(rv, "No tokensReceived");
+       }
+       
+       return true;
+    }
+}
