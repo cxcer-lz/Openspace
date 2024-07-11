dcc5e9bf5a4be2c716ecea791fadf960eaf73b44
cxcer-lz <zhwjleo@gmail.com>
2024-07-11 22:35:46 +0800

    Update MockERC20.sol

--- day8/test/MockERC20.sol
+++ day8/test/MockERC20.sol
@@ -1,1 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "../src/NftMarket.sol";
+
+contract MockERC20 is IERC20 {
+    string public name = "MockERC20";
+    string public symbol = "MERC";
+    uint8 public decimals = 18;
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(uint256 initialSupply) {
+        _totalSupply = initialSupply * 10**uint256(decimals);
+        _balances[msg.sender] = _totalSupply;
+    }
+
+    function totalSupply() external view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 value) external override returns (bool) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(_balances[msg.sender] >= value, "ERC20: transfer amount exceeds balance");
+
+        _balances[msg.sender] -= value;
+        _balances[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function allowance(address owner, address spender) external view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 value) external override returns (bool) {
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external override returns (bool) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(_balances[from] >= value, "ERC20: transfer amount exceeds balance");
+        require(_allowances[from][msg.sender] >= value, "ERC20: transfer amount exceeds allowance");
+
+        _balances[from] -= value;
+        _balances[to] += value;
+        _allowances[from][msg.sender] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+}
